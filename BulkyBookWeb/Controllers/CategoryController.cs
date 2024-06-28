d41279b30102dd2fd70eebf55410418dfc0e78c2
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-28 11:03:01 +0600

    CategoryController created

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public CategoryController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var categories = _context.Categories.ToList();
+        return View(categories);
+    }
+}
