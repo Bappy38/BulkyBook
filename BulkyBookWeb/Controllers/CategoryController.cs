b938bbb6006978c09878ddf8b3cd71ded3770981
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-28 11:48:55 +0600

    Validation added

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,6 +30,11 @@ public class CategoryController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult Create(Category category)
     {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
         _context.Categories.Add(category);
         _context.SaveChanges();
         return RedirectToAction("Index");
