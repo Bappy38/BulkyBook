579583e7780b5c398b27debc23ff1d97af2894ca
Bappy38 <iqbal.bappy.13@gmail.com>
2024-06-28 11:57:13 +0600

    Custom Validation added in Server Side

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -2,7 +2,7 @@
 
 namespace BulkyBookWeb.Models;
 
-public class Category
+public class Category : IValidatableObject
 {
     [Key]
     public int Id { get; set; }
@@ -10,4 +10,12 @@ public class Category
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.Equals(Name, DisplayOrder.ToString()))
+        {
+             yield return new ValidationResult("Name and DisplayOrder cannot be same.", new[] { nameof(Name), nameof(DisplayOrder) });
+        }
+    }
 }
